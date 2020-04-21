9b2ada3a77b4a0da0b836d904c820fb641b71f3e
MagHErmit <vladkakurin007@gmail.com>
2020-04-21 12:43:10 +1000

    Formating and small improvments

--- TechnoPointParser/TechnoPointParser.py
+++ TechnoPointParser/TechnoPointParser.py
@@ -1,98 +1,89 @@
+########-----------PARSE CONSTANTS----------########
+url = 'https://technopoint.ru/catalog/recipe/e351231ca6161134/2020-goda/'
+count = 10 # amount of objects(smartphones)
+url_api = 'https://technopoint.ru/ajax-state/price/'
+
+########-----------EXCEL CONSTANTS----------########
+topic = ['Наименование', 'Код товара', 'Цена', 'Ссылка на изображение']
+title_page = 'Смартфоны 2020'
+file_name = 'Смартфоны.xlsx'
+
+########-----------GET DATA----------########
 import requests
 from bs4 import BeautifulSoup
 import json
 
-url = 'https://technopoint.ru/catalog/recipe/e351231ca6161134/2020-goda/'
-
 s = requests.Session()
-s.headers.update({
+s.headers.update({ # Adding user-agent for robot-block
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36 OPR/67.0.3575.130'
     })
-r = s.get(url)
+response = s.get(url)
 
-soup = BeautifulSoup(r.text, features="lxml")
-list_products = soup.find_all('div', {'class' : 'catalog-item'})
 
+soup = BeautifulSoup(response.text, features="lxml")
+list_products = soup.find_all('div', {'class' : 'catalog-item'}) # Get basis for parse 
 
 names = []
 numbers = []
 links = []
 price = []
+data = {"data":"{\"type\":\"price\",\"containers\":["} # Base of argument for getting prices
 
-data = {"data":"{\"type\":\"price\",\"containers\":["}
-
-for i in range (0,10):
+for i in range (count): # Parse
     names.append(list_products[i].find('div', {'class': 'product-info__title-link'}).find('a').text)
     numbers.append(list_products[i].find('span', {'data-product-param' : 'code'}).text)
     links.append(list_products[i].find('img').get('data-src'))
-    data['data'] += "{\"id\":\"not-empty-%i\",\"data\":{\"product\":\"%s\"}}," % (i, list_products[i].get('data-guid'))
-
+    data['data'] += "{\"id\":\"not-empty-%i\",\"data\":{\"product\":\"%s\"}}," % (i, list_products[i].get('data-guid')) # Forming argument for API that get prices
+                                                                                                                        # "id"s can be random, but different values 
 data['data'] = data['data'][:-1]
-data['data'] += "]}"
+data['data'] += "]}" # End forming
     
+results = [names, numbers, price, links]
 
-results = [
-           names,
-           numbers,
-           price,
-           links
-          ]
-
-
-
-token = soup.find('meta', {'name' : 'csrf-token'}).get('content')
-
-s.headers.update({
+token = soup.find('meta', {'name' : 'csrf-token'}).get('content') # Parse token for request to API for prices
+s.headers.update({ # Adding header parametrs for getting access to API
     'x-csrf-token' : token,
     'origin' : 'https://technopoint.ru',
-    'referer' : 'https://technopoint.ru/catalog/recipe/e351231ca6161134/2020-goda/no-referrer',
+    'referer' : url+'no-referrer',
     'sec-fetch-mode' : 'cors',
     'content-type' : 'application/x-www-form-urlencoded',
     'x-requested-with' : 'XMLHttpRequest',
     })
 
+ans = json.loads(s.post(url_api, data = data).text) # Get JSON with prices
 
-
-ans = json.loads(s.post('https://technopoint.ru/ajax-state/price/', data = data).text)
-
-for price in ans['data']['states']:
+for price in ans['data']['states']: # Parse prices
     results[2].append(price['data']['current'])
 
-
-##################-----------------------WRITE TO EXCEL TABLE-----------------------##################
-
+########-----------WRITE TO EXCEL TABLE-----------########
 from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font, Side
+from openpyxl.styles import PatternFill, Font
 
-fill = PatternFill(fill_type='solid',start_color='FFD700')
-font = Font(name='Calibri',
-                    bold=True,
-                    )
-
+fill = PatternFill(fill_type='solid',start_color='FFD700') 
+                                                                                 
+font = Font(name='Calibri',bold=True,)
 
 wb = Workbook()
 ws = wb.active
-ws.title = 'Смартфоны 2020'
-topic = ['Наименование', 'Код товара', 'Цена', 'Ссылка на изображение']
-#for row in topic:
+ws.title = title_page
 ws.append(topic)
 
 for cellObj in ws['A1:D1']:
     for cell in cellObj:
         cell.fill = fill
         cell.font = font
-for i in range(0,10):
+for i in range(count): # Forming rows for appending
     row = []
     for j in range(0,4):
         row.append(results[j][i])
     ws.append(row)
 
-dims = {}
+dims = {} # Setting width depending on length of values
 for row in ws.rows:
     for cell in row:
         if cell.value:
             dims[cell.column_letter] = max((dims.get(cell.column, 0), len(cell.value)))
 for col, value in dims.items():
     ws.column_dimensions[col].width = value + 1
- 
-wb.save('Смартфоны.xlsx')
+
+wb.save(file_name)
